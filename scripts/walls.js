4283d2d85bf67ff04d8a49fbb7a4e8fd9add3200
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2023-09-03 08:53:02 -0400

    first additions

---
+++ scripts/walls.js
@@ -0,0 +1,15 @@
+let hugeCopperWall = extend(Wall, "huge-copper-wall", {
+    size: 3,
+    health: 2880,
+    requirements: ItemStack.with(Items.copper, 54),
+    buildCostMultiplier: 7,
+    research: Blocks.largeCopperWall
+})
+
+let hugeTitaniumWall = extend(Wall, "huge-copper-wall", {
+    size: 3,
+    health: 3960,
+    requirements: ItemStack.with(Items.titanium, 54),
+    buildCostMultiplier: 14,
+    research: Blocks.largeTitaniumWall
+})
