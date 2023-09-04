a522c344cf2740cff2a910595f61eac95d0d8be3
python-nerd-3 <117922845+python-nerd-3@users.noreply.github.com>
2023-09-04 14:47:56 -0400

    gotta appreciate anuke having goofy commit names
    "I HATE ANDROID GRADLE I HATE ANDROID GRADLE I HATE ANDROID GRADLE"
    
    next commit...
    
    "e"
    
    goofy commit names remind me how a real person made the game
    
    and not CORPORATECORPORATECORPORATE

--- scripts/main.js
+++ scripts/main.js
@@ -6,14 +6,18 @@ let hugeCopperWall = extend(Wall, "huge-copper-wall", {
     requirements: ItemStack.with(Items.copper, 54),
     buildCostMultiplier: 7,
     buildVisibility: BuildVisibility.shown,
-    research: Blocks.largeCopperWall
+    research: Blocks.copperWallLarge
 })
 
-let hugeTitaniumWall = extend(Wall, "huge-copper-wall", {
+let hugeTitaniumWall = extend(Wall, "huge-titanium-wall", {
     size: 3,
     health: 3960,
     requirements: ItemStack.with(Items.titanium, 54),
     buildCostMultiplier: 14,
     buildVisibility: BuildVisibility.shown,
-    research: Blocks.largeTitaniumWall
+    research: Blocks.titaniumWallLarge
 })
+
+// you're welcome
+copperWallLarge.buildCostMultiplier = 3
+titaniumWallLarge.buildCostMultiplier = 6
